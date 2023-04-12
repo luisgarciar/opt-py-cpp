23585708572e2024afe8656afbc2d80f9ffdd9b1
luisgarciar <luisgarciar@gmail.com>
2023-04-12 19:25:16 +0200

    [feat] Built basic version of binding function

--- src-cpp/bind/bindings.cpp
+++ src-cpp/bind/bindings.cpp
@@ -1,11 +1,15 @@
 //
 // Created by Luis - Work on 10.04.23.
-#include <../extern/pybind/include/pybind11.h>
+#include <pybind11/pybind11.h>
+#include "quad_function.hpp"
+
 namespace py = pybind11;
 
-PYBIND11_MODULE(example, m) {
-py::class_<Pet>(m, "Pet")
-.def(py::init<const std::string &>())
-.def("setName", &Pet::setName)
-.def("getName", &Pet::getName);
+PYBIND11_MODULE(quad_func, qf) {
+	qf.doc() = "pybind11 example plugin"; // optional module docstring
+
+	py::class_<quad_function>(qf, "quad_function")
+		.def(py::init<MatrixXd, VectorXd>())
+		.def("eval", &quad_function::eval)
+		.def("grad", &quad_function::grad);
 }
