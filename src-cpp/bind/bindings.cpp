90dbee897d0c79345eaf226c74a32d984c49bf59
luisgarciar <luisgarciar@gmail.com>
2023-04-14 20:40:01 +0200

    [build] Fix CMake build system and add setup.py installation script

--- src-cpp/bind/bindings.cpp
+++ src-cpp/bind/bindings.cpp
@@ -1,6 +1,7 @@
 //
 // Created by Luis - Work on 10.04.23.
 #include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
 #include "quad_function.hpp"
 
 namespace py = pybind11;
@@ -8,7 +9,7 @@ namespace py = pybind11;
 PYBIND11_MODULE(quad, m) {
 	m.doc() = "pybind11 example plugin"; // optional module docstring
 
-	py::class_<quadFunction>(m, "quad_function")
+	py::class_<quadFunction>(m, "function")
 		.def(py::init<MatrixXd, VectorXd>())
 		.def("eval", &quadFunction::eval)
 		.def("grad", &quadFunction::grad);
