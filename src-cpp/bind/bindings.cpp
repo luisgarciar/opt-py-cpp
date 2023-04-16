944c6ee823e3e6692dcdcee7b3beca7a28bbd986
luisgarciar <luisgarciar@gmail.com>
2023-04-16 16:34:29 +0200

    [fix] Change Eigen matrix type for compatibility with NumPy arrays

--- src-cpp/bind/bindings.cpp
+++ src-cpp/bind/bindings.cpp
@@ -9,7 +9,7 @@ namespace py = pybind11;
 PYBIND11_MODULE(quad, m){
 
 py::class_<quadFunction>(m, "function")
-.def(py::init< Eigen::Ref<Eigen::MatrixXd>, Eigen::Ref<Eigen::VectorXd>>())
+.def(py::init< py::EigenDRef<Eigen::MatrixXd>, py::EigenDRef<Eigen::VectorXd>>())
 .def("set_mat", &quadFunction::set_mat, py::arg("mat_in"))
 .def("set_b", &quadFunction::set_b, py::arg("b_in"))
 .def("get_mat", &quadFunction::get_mat, py::return_value_policy::reference)
