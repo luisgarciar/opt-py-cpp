a8641ac445c8f56aa0f19747196b96d3a2c9bfde
luisgarciar <luisgarciar@gmail.com>
2023-04-18 12:36:08 +0200

    [docs] Add path to config.py for auto generation of docs with sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,22 +6,33 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'simpleopt'
-copyright = '2023, Luis Garcia Ramos'
-author = 'Luis Garcia Ramos'
-release = '0.1'
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../src/simpleopt"))
+
+project = "simpleopt"
+copyright = "2023, Luis Garcia Ramos"
+author = "Luis Garcia Ramos"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", 'sphinx.ext.duration',
-              'sphinx.ext.doctest', 'sphinx.ext.autodoc','sphinx_rtd_theme']
+extensions = [
+    "myst_parser",
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
